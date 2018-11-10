a39a67eb00c67ea3819e8c932036af17535bfe0b
Damien Lespiau <damien@weave.works>
2018-11-10 13:26:50 +0000

    build: Split the CI script in 3 phases
    dep: install build deps not part of the build image
    install: install jk in $GOPATH/bin
    test: Run tests!

---
+++ Makefile
@@ -0,0 +1,26 @@
+.PHONY: std-install dep all jk install test
+
+all: jk
+
+jk: pkg/__std/lib/assets_vfsdata.go
+	GO111MODULE=on go build -o $@
+
+pkg/__std/lib/assets_vfsdata.go: std/build/std.js
+	GO111MODULE=on go generate ./pkg/__std/lib
+
+std/build/std.js: std/*.fbs std/*.js std/package.json
+	std/generate.sh
+	(cd std && npm run build)
+
+install: jk
+	cp jk `go env GOPATH`/bin
+
+# Pulls the std/node_modules directory
+std-install:
+	(cd std && npm install)
+
+# This target install build dependencies
+dep: std-install
+
+test:
+	./run-tests.sh
--- run-tests.sh
+++ run-tests.sh
@@ -1,27 +1,15 @@
 #!/bin/sh -e
 
-echo "==> Building std"
-(
-  cd std
-  npm install
-  mkdir -p build
-  npm run build
-)
-
 echo "==> Running std tests"
 (
   cd std
   npm test
 )
 
-echo "==> Building jk"
-export GO111MODULE=on
-go generate ./pkg/__std/lib/
-go install
+# Tests, both unit tests and integration tests under /tests
+echo "==> Running jk tests"
+GO111MODULE=on go test -v ./...
 
 echo "==> Checking committed generated files are up to date"
 git diff --exit-code
 
-# Tests, both unit tests and integration tests under /tests
-echo "==> Running jk tests"
-go test -v ./...
