7302014eb4c982250ff2133e583c3d4fae8d5c79
Damien Lespiau <damien@weave.works>
2018-11-10 10:37:27 +0000

    jk: Remove now unused goString function
    We always receive UTF-8 from the go side!

--- main.go
+++ main.go
@@ -1,36 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path"
 	"path/filepath"
-	"unicode/utf16"
-	"unicode/utf8"
 
 	"github.com/dlespiau/jk/pkg/std"
 
 	v8 "github.com/ry/v8worker2"
 )
-
-func goString(b []byte) string {
-	u16s := make([]uint16, 1)
-	ret := &bytes.Buffer{}
-	b8buf := make([]byte, 4)
-
-	lb := len(b)
-	for i := 0; i < lb; i += 2 {
-		u16s[0] = uint16(b[i]) + (uint16(b[i+1]) << 8)
-		r := utf16.Decode(u16s)
-		n := utf8.EncodeRune(b8buf, r[0])
-		ret.Write(b8buf[:n])
-	}
-
-	return ret.String()
-}
 
 func onMessageReceived(msg []byte) []byte {
 	return std.Execute(msg)
