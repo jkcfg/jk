8e7203b4cf48aef4e32daed9ba7fedcdf2e09506
Damien Lespiau <damien@weave.works>
2018-11-18 22:01:23 +0000

    examples: Fix log argument in nginx.js

--- nginx.js
+++ nginx.js
@@ -3,4 +3,4 @@ import std from 'std';
 
 const container = k.Container('nginx', 'nginx:1.15.4');
 const deployment = k.Deployment('nginx', 3, [container]);
-std.log(deployment, std.Format.YAML);
+std.log(deployment, { format: std.Format.YAML });
