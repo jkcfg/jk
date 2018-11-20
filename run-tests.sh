e2f2498d15631f9db1b83bd03b75ae76ac7301ea
Damien Lespiau <damien@weave.works>
2018-11-20 22:53:04 +0000

    tests: Allow vfsdata to be re-generated with different timestamps

--- run-tests.sh
+++ run-tests.sh
@@ -29,5 +29,12 @@ echo "==> Running jk tests"
 go test -v ./...
 
 echo "==> Checking committed generated files are up to date"
+
+# We allow pkg/__std/lib/assets_vfsdata.go to be re-generated, but with only
+# timestamp changed.
+vfsdata=pkg/__std/lib/assets_vfsdata.go
+changed=`git diff $vfsdata | grep ^[-+] | grep -v [ab]/$vfsdata | grep -v modTime: | wc -l`
+[ $changed -eq 0 ] && git checkout $vfsdata
+
 git diff --exit-code
 
