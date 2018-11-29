5063ec98cd8662ce6cad33793e5129c284319a08
Michael Bridgen <mikeb@squaremobius.net>
2018-11-29 12:01:23 +0000

    Make eslint happy

--- tests/test-read.js
+++ tests/test-read.js
@@ -1,4 +1,4 @@
 import std from 'std';
 
 const r = std.read('here is the URL');
-r.then(str => std.write("[OUT] "+str), err => std.write("[ERROR] "+err.toString()));
+r.then(str => std.write(`[OUT] ${str}`), err => std.write(`[ERROR] ${err.toString()}`));
