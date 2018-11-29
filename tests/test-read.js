43fa78ea31d8622ac1e6bac690f693b3f3fb8f4a
Michael Bridgen <mikeb@squaremobius.net>
2018-11-29 12:53:56 +0000

    WIP make read work

--- tests/test-read.js
+++ tests/test-read.js
@@ -1,4 +1,4 @@
 import std from 'std';
 
-const r = std.read('here is the URL');
-r.then(str => std.write(`[OUT] ${str}`), err => std.write(`[ERROR] ${err.toString()}`));
+const r = std.read('test-read.js.expected');
+r.then(std.write, err => std.write(`[ERROR] ${err.toString()}`));
