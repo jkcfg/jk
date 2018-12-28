22053839b11fec9d38f4080bc1b50ebdd01789af
Damien Lespiau <damien@weave.works>
2018-12-28 14:10:44 +0100

    std: Add a print function
    Log is designed to print out a JSON object and so isn't so convenient to print
    raw strings. std.print() helps there.

--- std/std.js
+++ std/std.js
@@ -1,9 +1,11 @@
+import { print } from 'std_print';
 import { log } from 'std_log';
 import { Format, write } from 'std_write';
 import { Encoding, read } from 'std_read';
 import { info, dir } from 'std_fs';
 
 export default {
+  print,
   log,
   Format,
   write,
--- tests/test-std-ts/test.ts
+++ tests/test-std-ts/test.ts
@@ -1,5 +1,6 @@
 import std from 'std';
 
+std.print('success');
 std.log('success');
 std.write('success', '');
 std.write('success', '', { format: std.Format.JSON, indent: 2, override: false });
