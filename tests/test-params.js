8c18edf7c6d2f1528a9334dcd3d6a6e83ad3e6e5
Damien Lespiau <damien@weave.works>
2019-01-02 20:18:54 +0100

    param: Test default parameter value shine though

--- tests/test-params.js
+++ tests/test-params.js
@@ -17,3 +17,23 @@ std.log({
   myString: s,
   myObject: o,
 });
+
+// Test default values are shining through when the parameters aren't
+// specified.
+const bD = std.param.Boolean('myBooleanD', false);
+const nD = std.param.Number('myNumberD', 3.14);
+const sD = std.param.String('myStringD', 'foo');
+const oD = std.param.Object('myObjectD', {
+  s: 'bar',
+  b: true,
+  o: {
+    xxx: 'yyy',
+  },
+});
+
+std.log({
+  myBoolean: bD,
+  myNumber: nD,
+  myString: sD,
+  myObject: oD,
+});
