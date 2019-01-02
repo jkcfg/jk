eacb6686c21f774365e6e57e5df4ad210a17cfb7
Damien Lespiau <damien@weave.works>
2019-01-02 19:06:59 +0100

    run: Add options to provide parameters at run-time

--- run.go
+++ run.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"path"
+	"strconv"
+	"strings"
 
 	"github.com/jkcfg/jk/pkg/deferred"
 	"github.com/jkcfg/jk/pkg/resolve"
@@ -12,6 +15,7 @@ import (
 
 	v8 "github.com/ry/v8worker2"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var runCmd = &cobra.Command{
@@ -21,12 +25,72 @@ var runCmd = &cobra.Command{
 	Run:   run,
 }
 
+type paramsOption struct {
+	params *std.Params
+	kind   std.ParamKind
+}
+
+func (p *paramsOption) String() string {
+	return ""
+}
+
+func (p *paramsOption) Set(s string) error {
+	parts := strings.Split(s, "=")
+	if len(parts) != 2 {
+		return errors.New("input parameters are of the form name=value")
+	}
+	path := parts[0]
+	v := parts[1]
+
+	switch p.kind {
+	case std.ParamKindBoolean:
+		b, err := strconv.ParseBool(v)
+		if err != nil {
+			return fmt.Errorf("could not parse '%s' as a boolean", v)
+		}
+		p.params.SetBool(path, b)
+	case std.ParamKindNumber:
+		n, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return fmt.Errorf("could not parse '%s' as a float64", v)
+		}
+		p.params.SetNumber(path, n)
+	case std.ParamKindString:
+		p.params.SetString(path, v)
+	case std.ParamKindObject:
+		o, err := std.NewParamsFromJSON(strings.NewReader(v))
+		if err != nil {
+			return fmt.Errorf("could not parse JSON '%s': %v", v, err)
+		}
+		p.params.SetObject(path, o)
+	}
+
+	return nil
+}
+
+func (p *paramsOption) Type() string {
+	return "name=value"
+}
+
 var runOptions struct {
 	outputDirectory string
+	parameters      std.Params
+}
+
+func parameters(kind std.ParamKind) pflag.Value {
+	return &paramsOption{
+		params: &runOptions.parameters,
+		kind:   kind,
+	}
 }
 
 func init() {
+	runOptions.parameters = std.NewParams()
 	runCmd.PersistentFlags().StringVarP(&runOptions.outputDirectory, "output-directory", "o", "", "where to output generated files")
+	runCmd.PersistentFlags().Var(parameters(std.ParamKindBoolean), "pb", "boolean input parameter")
+	runCmd.PersistentFlags().Var(parameters(std.ParamKindNumber), "pn", "number input parameter")
+	runCmd.PersistentFlags().Var(parameters(std.ParamKindString), "ps", "string input parameter")
+	runCmd.PersistentFlags().Var(parameters(std.ParamKindObject), "po", "object input parameter")
 	jk.AddCommand(runCmd)
 }
 
@@ -43,6 +107,7 @@ type exec struct {
 
 func (e *exec) onMessageReceived(msg []byte) []byte {
 	return std.Execute(msg, e.worker, std.ExecuteOptions{
+		Parameters:      runOptions.parameters,
 		OutputDirectory: runOptions.outputDirectory,
 	})
 }
