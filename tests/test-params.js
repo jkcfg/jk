cf6c13dccd585cd0ad8308d2274101eb2b19d8be
Damien Lespiau <damien@weave.works>
2019-01-02 19:07:19 +0100

    tests: Add a test ensuring we can pass params from the command line

---
+++ tests/test-params.js
@@ -0,0 +1,19 @@
+import std from 'std';
+
+const b = std.param.Boolean('myBoolean', false);
+const n = std.param.Number('myNumber', 2.0);
+const s = std.param.String('myString', 'foo');
+const o = std.param.Object('myObject', {
+  s: 'bar',
+  b: true,
+  o: {
+    xxx: 'yyy',
+  },
+});
+
+std.log({
+  myBoolean: b,
+  myNumber: n,
+  myString: s,
+  myObject: o,
+});
---
+++ tests/test-params.js.cmd
@@ -0,0 +1,1 @@
+jk run --pb myBoolean=true --pn myNumber=2.0 --ps myString=success --po myObject={"message":"success","o":{"foo":"bar"}} %f
