bd4f19848ef668677ea6e7f2221b8c7c17f693e9
Michael Bridgen <mikeb@squaremobius.net>
2019-01-15 23:06:18 +0000

    Test more NPM resolutioning

--- tests/test-npm-imports.js
+++ tests/test-npm-imports.js
@@ -6,3 +6,8 @@ std.write(msg1, 'test1.json');
 import msg2 from 'testcase/submodule'; // node_modules/testcase/submodule.js
 std.write(msg2, 'test2.json');
 
+import msg3 from 'testcase/indirect'; // node_modules/testcase/indirect.js imports ./test3
+std.write(msg3, 'test3.json');
+
+import msg4 from 'testcase/subdir'; // node_modules/testcase/subdir/package.json specifies test4.js in its `module` field
+std.write(msg4, 'test4.json');
