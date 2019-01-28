20a7f43add709b43135fe75d4d0f7383318a4c9b
Michael Bridgen <mikeb@squaremobius.net>
2019-01-28 22:58:59 +0000

    Wrap long comments in test for humans/eslint

--- tests/test-npm-imports.js
+++ tests/test-npm-imports.js
@@ -4,18 +4,24 @@
 /* eslint "import/newline-after-import": [0] */
 import std from '@jkcfg/std';
 
-import msg1 from 'testcase'; // node_modules/testcase/index.js
+// node_modules/testcase/index.js
+import msg1 from 'testcase';
 std.write(msg1, 'test1.json');
 
-import msg2 from 'testcase/submodule'; // node_modules/testcase/submodule.js
+// node_modules/testcase/submodule.js
+import msg2 from 'testcase/submodule';
 std.write(msg2, 'test2.json');
 
-import msg3 from 'testcase/indirect'; // node_modules/testcase/indirect.js imports ./test3
+// node_modules/testcase/indirect.js imports ./test3
+import msg3 from 'testcase/indirect';
 std.write(msg3, 'test3.json');
 
-import msg4 from 'testcase/subdir'; // node_modules/testcase/subdir/package.json specifies test4.js in its `module` field
+// node_modules/testcase/subdir/package.json specifies test4.js in its
+// `module` field
+import msg4 from 'testcase/subdir';
 std.write(msg4, 'test4.json');
 
+// node_modules/testcase/vendor.js imports from 'vendor', which is to
+// be found at node_modules/testcase/node_modules/vendored/index.js
 import msg5 from 'testcase/vendor';
-// node_modules/testcase/vendor.js imports from 'vendor', which is to be found at node_modules/testcase/node_modules/vendored/index.js
 std.write(msg5, 'test5.json');
