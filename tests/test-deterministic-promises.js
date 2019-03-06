a333749a75a7416a0e975604193378918e42dc3a
Damien Lespiau <damien.lespiau@gmail.com>
2019-03-06 11:59:43 +0000

    tests: Make test-deterministic-promises more presentable
    We the info capturing i in the loop works (because of the block scoping from
    let), we can refine a bit the test, also removing the unnecessary encoding
    argument to std.read.
    
    I tested it with a previous version of jk and the test still shows the "random"
    behaviour.

--- tests/test-deterministic-promises.js
+++ tests/test-deterministic-promises.js
@@ -1,11 +1,9 @@
 import std from '@jkcfg/std';
 
 const resolvedOrder = [];
-const checkRead = i => std.read('success.json', { encoding: std.Encoding.String }).then(() => resolvedOrder.push(i));
-
 const promises = [];
 for (let i = 0; i < 100; i += 1) {
-  promises.push(checkRead(i));
+  promises.push(std.read('success.json').then(() => resolvedOrder.push(i)));
 }
 
 Promise.all(promises).then(() => std.log(resolvedOrder.join(' ')));
