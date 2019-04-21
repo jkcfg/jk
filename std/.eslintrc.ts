abeb8c040259e346c4981c94f795cdac0969c0ec
Damien Lespiau <damien.lespiau@gmail.com>
2019-04-21 10:16:53 +0100

    std: Implement linting for typescript

---
+++ std/.eslintrc.ts
@@ -0,0 +1,37 @@
+{
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["jest", "import", "@typescript-eslint"],
+  "extends": ["airbnb-base", "plugin:@typescript-eslint/recommended", "plugin:import/typescript"],
+  "globals": {
+    "V8Worker2": true
+  },
+  "settings": {
+     "import/resolver": {
+        "node": {
+          "paths": ["."]
+        }
+     }
+  },
+  "env": {
+    "jest/globals": true
+  },
+  "rules": {
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 2, {"SwitchCase": 0, "CallExpression": {"arguments": "first"}}],
+    "no-undef": "off",
+    "@typescript-eslint/no-explicit-any": off,
+    "no-continue": 0,
+    "prefer-const": ["error", {"destructuring": "all"}],
+    "import/prefer-default-export": 0,
+    "import/no-unresolved": [
+      2,
+      {
+        "ignore": [
+          "^@jkcfg/std$"
+        ]
+      }
+    ],
+    "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
+    "no-use-before-define": ["error", { "functions": false }]
+  }
+}
