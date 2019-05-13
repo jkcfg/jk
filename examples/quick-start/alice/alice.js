0d0ffbfcb0c2549114d5e0e4181e91d61a32ec31
Damien Lespiau <damien.lespiau@gmail.com>
2019-05-13 16:47:39 +0100

    generate: Add a simple example

---
+++ examples/quick-start/alice/alice.js
@@ -0,0 +1,17 @@
+// Define a developer.
+const alice = {
+    name: 'Alice',
+    beverage: 'Club-Mate',
+    monitors: 2,
+    languages: [
+        'python',
+        'haskell',
+        'c++',
+        '68k assembly', // Alice is cool like that!
+    ],
+};
+
+// Write the developer description as YAML.
+export default [
+  { value: alice, file: `developers/${alice.name.toLowerCase()}.yaml` },
+];
