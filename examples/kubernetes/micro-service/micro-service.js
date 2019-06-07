ace5ea17168e9c5d70f68fc59f8ade3b946b8080
Damien Lespiau <damien.lespiau@gmail.com>
2019-06-07 15:50:59 +0100

    example: Generate a ConfigMap containing a service dashboard

--- examples/kubernetes/micro-service/micro-service.js
+++ examples/kubernetes/micro-service/micro-service.js
@@ -1,14 +1,21 @@
+import { unparse, Format } from '@jkcfg/std';
 import * as param from '@jkcfg/std/param';
 import {
-  Namespace, Deployment, Service, Ingress,
+  Namespace, Deployment, Service, Ingress, ConfigMap,
 } from './kubernetes';
+import { Dashboard } from './dashboard';
 
 const service = param.Object('service');
 const ns = service.namespace;
+
+const dashboards = {
+  dashboard: unparse(Dashboard(service), Format.JSON),
+};
 
 export default [
   { file: `${ns}-ns.yaml`, value: Namespace(service) },
   { file: `${ns}/${service.name}-deploy.yaml`, value: Deployment(service) },
   { file: `${ns}/${service.name}-svc.yaml`, value: Service(service) },
   { file: `${ns}/${service.name}-ingress.yaml`, value: Ingress(service) },
+  { file: `${ns}/${service.name}-dashboards-cm.yaml`, value: ConfigMap(service, `${service.name}-dashboards`, dashboards) },
 ];
