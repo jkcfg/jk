8faa91c6e99bea89d1cc1601910865fa17dcc5f6
Damien Lespiau <damien.lespiau@gmail.com>
2019-06-21 11:38:30 +0100

    examples: Make a kubernetes import more display friendly
    I'd like to show that example on the homepage, shorter will look better.

--- examples/kubernetes/micro-service/micro-service.js
+++ examples/kubernetes/micro-service/micro-service.js
@@ -1,8 +1,6 @@
 import { unparse, Format } from '@jkcfg/std';
 import * as param from '@jkcfg/std/param';
-import {
-  Namespace, Deployment, Service, Ingress, ConfigMap,
-} from './kubernetes';
+import * as k from './kubernetes';
 import { Dashboard } from './dashboard';
 import { PrometheusRule } from './alert';
 
@@ -14,10 +12,10 @@ const dashboards = {
 };
 
 export default [
-  { file: `${ns}-ns.yaml`, value: Namespace(service) },
-  { file: `${ns}/${service.name}-deploy.yaml`, value: Deployment(service) },
-  { file: `${ns}/${service.name}-svc.yaml`, value: Service(service) },
-  { file: `${ns}/${service.name}-ingress.yaml`, value: Ingress(service) },
-  { file: `${ns}/${service.name}-dashboards-cm.yaml`, value: ConfigMap(service, `${service.name}-dashboards`, dashboards) },
+  { file: `${ns}-ns.yaml`, value: k.Namespace(service) },
+  { file: `${ns}/${service.name}-deploy.yaml`, value: k.Deployment(service) },
+  { file: `${ns}/${service.name}-svc.yaml`, value: k.Service(service) },
+  { file: `${ns}/${service.name}-ingress.yaml`, value: k.Ingress(service) },
+  { file: `${ns}/${service.name}-dashboards-cm.yaml`, value: k.ConfigMap(service, `${service.name}-dashboards`, dashboards) },
   { file: `${ns}/${service.name}-prometheus-rule.yaml`, value: PrometheusRule(service) },
 ];
