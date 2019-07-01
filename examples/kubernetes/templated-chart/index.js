a030dac755057a237fc169a88f4712f45eaa0977
Michael Bridgen <mikeb@squaremobius.net>
2019-07-01 19:34:53 +0100

    Use a submodule in templated-chart, and explain
    This example is better if the chart module is self-contained. This
    commit separates the code that defines the chart from the code that
    instantiates it, putting the former in a submodule.

--- examples/kubernetes/templated-chart/index.js
+++ examples/kubernetes/templated-chart/index.js
@@ -1,19 +1,6 @@
 // Example of a Helm chart analogue, using handlebars
 
-import { generateChart, loadModuleTemplates } from '@jkcfg/kubernetes/chart';
-import * as resource from '@jkcfg/std/resource';
 import * as param from '@jkcfg/std/param';
-import handlebars from 'handlebars/lib/handlebars';
+import chart from './chart';
 
-const defaults = {
-  name: 'helloworld',
-  app: 'hello',
-  image: {
-    repository: 'weaveworks/helloworld',
-    tag: 'v1'
-  }
-};
-
-const templates = loadModuleTemplates(handlebars.compile, resource);
-
-export default generateChart(templates, defaults, param);
+export default chart(param);
