47f8bc64e59125ee9a02c47ab516f595eda8c891
Damien Lespiau <damien.lespiau@gmail.com>
2019-07-11 16:17:50 +0100

    examples: Add an end to end test able to run examples
    And convert quick-start/alice to use this new mechanism!

---
+++ examples/examples_test.go
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"path/filepath"
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/jkcfg/jk/pkg/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func listTestFiles(t *testing.T) []string {
+	var files []string
+
+	files, err := filepath.Glob("*/*/test.cmd")
+	assert.NoError(t, err)
+
+	sort.Strings(files)
+	return files
+}
+
+func wd(path string) string {
+	parts := strings.Split(path, "/")
+	return strings.Join(parts[:2], "/")
+}
+
+func cmd(script string) string {
+	return script
+}
+
+func expectedOutputFile(script string) string {
+	return filepath.Join(wd(script), "test.expected")
+}
+
+func TestExamples(t *testing.T) {
+	files := listTestFiles(t)
+
+	for _, file := range files {
+		test := test.New(file, test.Options{
+			Name:               wd(file),
+			WorkingDirectory:   wd(file),
+			CommandFile:        cmd,
+			ExpectedOutputFile: expectedOutputFile,
+		})
+		t.Run(test.Name(), func(t *testing.T) {
+			test.Run(t)
+		})
+	}
+}
