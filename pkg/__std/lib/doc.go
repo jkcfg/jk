aa0edb57ce8af406ac043c3ef38b8ac8bdb021f3
Michael Bridgen <mikeb@squaremobius.net>
2019-08-19 15:31:09 +0100

    Correct comment on bundling of JS libs

--- pkg/__std/lib/doc.go
+++ pkg/__std/lib/doc.go
@@ -1,4 +1,5 @@
 //go:generate go run -mod=readonly -tags=dev assets_generate.go
 
-// Package lib contains the JS std library, bundled and minified.
+// Package lib contains the JS std library, i.e., the std/dist
+// directory.
 package lib
