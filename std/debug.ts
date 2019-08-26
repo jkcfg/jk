837cd461ffcc8f200ce600ba802b7610afdf8098
Michael Bridgen <mikeb@squaremobius.net>
2019-08-26 15:09:46 +0100

    Port debug module to TypeScript

--- std/debug.ts
+++ std/debug.ts
@@ -1,6 +1,6 @@
 import { valueFromUTF8Bytes } from './internal/data';
 import { RPC } from './internal/rpc';
 
-export function echo(...args) {
+export function echo(...args: any[]): Promise<any[]> {
   return RPC('debug.echo', ...args).then(valueFromUTF8Bytes);
 }
