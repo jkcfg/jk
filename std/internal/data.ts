9adffc0537fc5bdaff41e108348dea95192aa6ae
Michael Bridgen <mikeb@squaremobius.net>
2019-10-12 13:51:12 +0100

    Enable large string values from runtime
    When reading a file, the value is (usually) sent back to JavaScript
    encoded as UTF16, to make it easier to turn into a string --
    JavaScript uses UTF16, and `String.fromCharCode` expects 16 bit values
    (in unicode taxonomy: code units).
    
    `String.fromCharCode` is tremendously convenient for this purpose,
    _except_ that it accepts varargs (rather than an array of code units),
    and there's a practical limit to the number of varargs that can be
    supplied. Reading file larger than a few tens of kilobytes will exceed
    that limit.
    
    So: do the conversion of bytes to strings in chunks, sticking to a
    conservative number of varargs at a time. This also changes from
    `String.fromCodePoint` to `String.fromCharCode` (which expects code
    _units_, i.e., the 16-bit values, which is what we have).
    
    I have not made `stringFromUTF8Bytes` work the same way; it is used
    for decoding values _constructed_ by the runtime; e.g., the results of
    RPC calls. These are less likely to be large in size -- though of
    course the possibility exists, and it could be handled the same way.

--- std/internal/data.ts
+++ std/internal/data.ts
@@ -2,14 +2,28 @@ export type Transform = (x: Uint8Array) => any;
 
 export const ident : Transform = (x: Uint8Array): any => x;
 
-function uint8ToUint16Array(bytes: Uint8Array): Uint16Array {
-  return new Uint16Array(bytes.buffer, bytes.byteOffset, bytes.byteLength / 2);
+// There's a practical limit to the number of arguments that can be
+// given to a function, either via apply or spread syntax. This
+// function chunks the conversion using a conservative number of
+// arguments to String.fromCharCode; the exact limit for v8 appears to
+// be 65535, but evidence elsewhere suggests a smaller value is
+// better:
+// https://github.com/google/closure-library/commit/da353e0265ea32583ea1db9e7520dce5cceb6f6a
+function stringFromUTF16CharCodes(bytes: ArrayBuffer, byteOffset: number, byteLength: number): string {
+  const chunk = 8192 * 2;
+  let result = '';
+  for (let i = 0; i < byteLength; i += chunk) {
+    const len = ((i + chunk) > byteLength) ? (byteLength - i)/2 : chunk / 2;
+    const codes = new Uint16Array(bytes, byteOffset + i, len);
+    result += String.fromCharCode.apply(null, codes);
+  };
+  return result;
 }
 
 // From the runtime, we get various encodings back depending on the
 // type in the schema, some of which will be post-processed by
 // flatbuffers.
-export const stringFromUTF16Bytes = (bytes: Uint8Array): string => String.fromCodePoint(...uint8ToUint16Array(bytes));
-export const stringFromUTF8Bytes = (bytes: Uint8Array): string => String.fromCodePoint(...bytes);
+export const stringFromUTF16Bytes = (bytes: Uint8Array): string => stringFromUTF16CharCodes(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+export const stringFromUTF8Bytes = (bytes: Uint8Array): string => String.fromCharCode.apply(null, bytes);
 export const valueFromUTF8Bytes = (bytes: Uint8Array): any => JSON.parse(stringFromUTF8Bytes(bytes));
 export const valueFromUTF16Bytes = (bytes: Uint8Array): any => JSON.parse(stringFromUTF16Bytes(bytes));
