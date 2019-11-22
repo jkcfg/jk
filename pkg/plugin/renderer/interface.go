f9f1638488336231d3ec771b3f6ea63a7516bd2f
Damien Lespiau <damien.lespiau@gmail.com>
2019-11-22 13:36:29 +0000

    Update pkg/plugin/renderer/interface.go
    Co-Authored-By: Michael Bridgen <mikeb@squaremobius.net>

--- pkg/plugin/renderer/interface.go
+++ pkg/plugin/renderer/interface.go
@@ -14,7 +14,7 @@ var RendererV1 = plugin.HandshakeConfig{
 // Maps are sent and received as json objects in byte arrays. It's a lot simpler
 // to send a byte array through RPC than trying to have the message encoding try
 // to serialize a map, especially because we can hand over the byte array to v8
-// directory and unmarshal the JSON there.
+// directly and unmarshal the JSON there.
 //
 // Both the input and the result byte arrays are JSON documents serialized as
 // utf8 strings.
