4c81091d0c0872e4e660b08239ac78c5e3682da5
Michael Bridgen <mikeb@squaremobius.net>
2019-11-29 12:06:33 +0000

    Implementations of union filesystem

---
+++ pkg/image/imagefs.go
@@ -0,0 +1,139 @@
+package image
+
+// Treat a set of layers as a filesystem, for e.g., putting in the
+// module search path.
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+// Overlay is a read-only filesystem formed with a list of layers
+// representing diffs. It is designed to behave like a filesystem such
+// as those used in containers. The layers are given uppermost first
+// (at index 0).
+//
+//     https://www.kernel.org/doc/Documentation/filesystems/overlayfs.txt
+// is the inspiration for OCI image layers:
+//
+// > An overlay filesystem combines two filesystems - an 'upper' filesystem
+// > and a 'lower' filesystem.  When a name exists in both filesystems, the
+// > object in the 'upper' filesystem is visible while the object in the
+// > 'lower' filesystem is either hidden or, in the case of directories,
+// > merged with the 'upper' object.
+type Overlay struct {
+	layers []http.FileSystem
+}
+
+// NewOverlay constructs an overlay filesystem given the layers
+func NewOverlay(layers ...http.FileSystem) *Overlay {
+	return &Overlay{
+		layers: layers,
+	}
+}
+
+// Open implements http.FileSystem#Open for the overlay filesystem
+func (o *Overlay) Open(path string) (http.File, error) {
+	for i, fs := range o.layers {
+		file, err := fs.Open(path)
+		if err != nil {
+			continue
+		}
+
+		stat, err := file.Stat()
+		if err != nil {
+			continue
+		}
+
+		// If it's not a directory, this hides any lower entries
+		if !stat.IsDir() {
+			return file, nil
+		}
+
+		// It _is_ a directory. This will have the entries of all
+		// lower directory entries.
+		return &dir{
+			path:  path,
+			File:  file,
+			lower: o.layers[i+1:],
+		}, nil
+	}
+
+	return nil, &os.PathError{Op: "open", Path: path, Err: os.ErrNotExist}
+}
+
+// dir represents a directory in an overlay filesystem
+type dir struct {
+	http.File // the uppermost file
+	path      string
+	lower     []http.FileSystem
+	// readdir state
+	readdir []os.FileInfo
+}
+
+// Readdir implements http.File#Readdir for a directory in an overlay
+// filesystem.
+func (d *dir) Readdir(count int) ([]os.FileInfo, error) {
+	// return the appropriate combination of slice and error, which
+	// depends on the requested count. This assumes d.readdir has been
+	// calculated.
+	appropriate := func(err error) ([]os.FileInfo, error) {
+		// This is a minor fudge: it may raise an error earlier than
+		// it would otherwise been seen when using count > 0.
+		if count > 0 {
+			if err != nil {
+				return nil, err
+			}
+			if len(d.readdir) == 0 {
+				return nil, io.EOF
+			}
+			result := d.readdir[:count]
+			d.readdir = d.readdir[count:]
+			return result, nil
+		}
+		return d.readdir, err
+	}
+
+	// compile all the entries first
+	if d.readdir == nil {
+		seen := map[string]struct{}{}
+		record := func(infos []os.FileInfo) {
+			for _, info := range infos {
+				name := info.Name()
+				if _, ok := seen[name]; !ok {
+					seen[name] = struct{}{}
+					d.readdir = append(d.readdir, info)
+				}
+			}
+		}
+
+		infos, err := d.File.Readdir(0)
+		record(infos)
+		if err != nil {
+			return appropriate(err)
+		}
+
+		for _, layer := range d.lower {
+			file, err := layer.Open(d.path)
+			if err != nil { // no file, check the next layer
+				continue
+			}
+			info, err := file.Stat()
+			if err != nil {
+				return appropriate(err)
+			}
+			if !info.IsDir() {
+				// a dead end, stop here.
+				return appropriate(nil)
+			}
+			infos, err := file.Readdir(0)
+			record(infos)
+			if err != nil {
+				return appropriate(err)
+			}
+		}
+	}
+
+	return appropriate(nil)
+}
---
+++ pkg/image/imagefs_test.go
@@ -0,0 +1,146 @@
+package image
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/tools/godoc/vfs/httpfs"
+	"golang.org/x/tools/godoc/vfs/mapfs"
+)
+
+func newMapFS(files map[string]string) http.FileSystem {
+	return httpfs.New(mapfs.New(files))
+}
+
+// Test the simplest case: can I read a file from a single layer
+func TestSingleLayerSingleFile(t *testing.T) {
+	files := map[string]string{"foo": "here is the text"}
+	fs := NewOverlay(newMapFS(files))
+	assert.NotNil(t, fs)
+	file, err := fs.Open("foo")
+	assert.NoError(t, err)
+	assert.NotNil(t, file)
+	bytes, err := ioutil.ReadAll(file)
+	assert.NoError(t, err)
+	assert.Equal(t, files["foo"], string(bytes))
+}
+
+// Test the second simplest case: can I read a file that is not in the
+// uppermost layer.
+func TestSingleFile(t *testing.T) {
+	files1 := map[string]string{"bar": "not the text"}
+	files2 := map[string]string{"foo": "here is the text"}
+
+	fs := NewOverlay(newMapFS(files1), newMapFS(files2))
+	assert.NotNil(t, fs)
+	file, err := fs.Open("foo")
+	assert.NoError(t, err)
+	assert.NotNil(t, file)
+	bytes, err := ioutil.ReadAll(file)
+	assert.NoError(t, err)
+	assert.Equal(t, files2["foo"], string(bytes))
+}
+
+// Mainly a check that I know what I'm doing when using mapfs
+func TestMapReaddir(t *testing.T) {
+	files1 := map[string]string{
+		"dir/foo": "unimportant",
+		"dir/bar": "unimportant",
+	}
+	fs := newMapFS(files1)
+	assert.NotNil(t, fs)
+
+	dir, err := fs.Open("/dir")
+	assert.NoError(t, err)
+
+	info, err := dir.Stat()
+	assert.NoError(t, err)
+	assert.True(t, info.IsDir())
+
+	_, err = dir.Read(nil)
+	assert.Error(t, err)
+
+	infos, err := dir.Readdir(0)
+	assert.NoError(t, err)
+	assert.Len(t, infos, 2)
+}
+
+// Test that readdir will return all the appropriate entries, that is
+// directory entries across all layers, including one that does not
+// contain the directory, and one that defines the file in question as
+// a non-directory (stopping the search).
+func TestReaddir(t *testing.T) {
+	files1 := map[string]string{
+		"dir/foo": "unimportant",
+		"dir/bar": "unimportant",
+	}
+	// a layer in which no such dir exists
+	files2 := map[string]string{
+		"other/foo": "unimportant",
+	}
+	files3 := map[string]string{
+		"dir/baz": "unimportant",
+		"dir/bop": "unimportant",
+	}
+	files4 := map[string]string{
+		"dir": "dead end",
+	}
+	files5 := map[string]string{
+		"dir/boo": "unimportant",
+	}
+	fs := NewOverlay(
+		newMapFS(files1),
+		newMapFS(files2),
+		newMapFS(files3),
+		newMapFS(files4),
+		newMapFS(files5),
+	)
+	assert.NotNil(t, fs)
+
+	dir, err := fs.Open("/dir")
+	assert.NoError(t, err)
+
+	info, err := dir.Stat()
+	assert.NoError(t, err)
+	assert.True(t, info.IsDir())
+
+	_, err = dir.Read(nil)
+	assert.Error(t, err)
+
+	infos, err := dir.Readdir(0)
+	assert.NoError(t, err)
+	assert.Len(t, infos, 4)
+}
+
+// Test that readdir will discard duplicate entries; these would not
+// correspond to files that can be opened, since the file is present
+// on an upper layer.
+func TestReadDuplicates(t *testing.T) {
+	files1 := map[string]string{
+		"dir/foo": "unimportant",
+		"dir/bar": "unimportant",
+	}
+	files2 := map[string]string{
+		"dir/foo": "overridden by upper layer",
+		"dir/baz": "fresh file",
+	}
+
+	fs := NewOverlay(newMapFS(files1), newMapFS(files2))
+	assert.NotNil(t, fs)
+
+	dir, err := fs.Open("/dir")
+	assert.NoError(t, err)
+
+	info, err := dir.Stat()
+	assert.NoError(t, err)
+	assert.True(t, info.IsDir())
+
+	_, err = dir.Read(nil)
+	assert.Error(t, err)
+
+	infos, err := dir.Readdir(0)
+	assert.NoError(t, err)
+	assert.Len(t, infos, 3)
+}
